01fc4f11c4c62be0be953a2f9ea9039688058000
Hudson Tavares <hudson.tavares@gmail.com>
2017-05-18 15:21:31 -0300

    Added styles for the SearchBox directive

--- scripts/controllers/SearchBox.js
+++ scripts/controllers/SearchBox.js
@@ -1,16 +1,32 @@
 define (["utils/index"], function (Utils) {
 
-    var SearchBox = function (MessageService) {
+    var SearchBox = function (MessageService, $window) {
       this.params = {};
+      var aliasList = {};
       this.search = function (event) {
-        Utils.preventEvent(event);
-        MessageService.trigger("searchbox:search", this.params);
-        return true;
+        try {
+          Utils.preventEvent(event);
+          this.check(this.params);
+          MessageService.trigger("searchbox:search", this.params);
+        }
+        catch (error) {
+          $window.alert("Please provide a " + error + ".");
+        }
       };
+      this.init = function (index, value, alias) {
+        this.params[index] = value;
+        aliasList[index] = alias;
+      };
+      this.check = function(params) {
+        Object.keys(params).forEach( function (index) {
+          if (params[index] === "")
+            throw aliasList[index];
+        });
+      }
     };
 
     /* Dependencies injection */
-    SearchBox.$inject = ["MessageService"];
+    SearchBox.$inject = ["MessageService", "$window"];
 
     /* Assigns directive to an app instance */
     SearchBox.assign = function (app) {
